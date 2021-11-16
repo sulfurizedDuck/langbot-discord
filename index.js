a051fe36f69bec1170c83e962fc1890b3e43ccbe
Albertus Hermanto <albertus.hermanto@gdplabs.id>
2021-11-16 20:56:56 +0700

    Added replies for direct messages

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const BondQueryProcessor = require('./query/BondQueryProcessor');
 const client = new Client({
   intents: [
     Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.DIRECT_MESSAGES,
   ],
 });
 
